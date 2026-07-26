06513914361c6e62f72d600e1f682fefe33a1436
ShihabRacs <racs.chisty@gmail.com>
2026-07-27 01:12:10 +0200

    Require admin login to access QR validator

--- routes/validator.js
+++ routes/validator.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { requireAdmin } = require('../middleware/auth');
+
+router.use(requireAdmin);
 
 router.get('/', (req, res) => {
   res.render('validator/scan', { title: 'QR Validator' });
